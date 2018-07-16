5292783e2b3e19e4458f93a3d8868e9e7df3b577
Ian Pelow <ipelow@kent.edu>
2018-07-16 08:23:39 -0400

    tags edit

--- src/main/java/com/bridleplates/fullstackreviewsite/Tag.java
+++ src/main/java/com/bridleplates/fullstackreviewsite/Tag.java
@@ -1,10 +1,12 @@
 package com.bridleplates.fullstackreviewsite;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 
 @Entity
@@ -13,17 +15,56 @@ public class Tag {
 	@Id
 	@GeneratedValue
 	private Long id;
+	
 	private String name;
-
-	@ManyToMany(mappedBy = "tags")
+	private String imgUrl;
+	private String imgUrl2;
+	private String imgUrl3;
+	
+	@Lob
+	private String description;
+	
+	@ManyToMany
 	private Collection<Breed> breeds;
-
-	protected Tag() {
-	}
-
+	
+	
+	public Tag () {}
+	
 	public Tag(String name) {
-		this.name = name;
+		this.name = name;		
 	}
+
+	public Tag(String name, String imgUrl, String imgUrl2, String imgUrl3, String description, Breed... breeds
+			) {
+		this.name = name;
+		this.imgUrl = imgUrl;
+		this.imgUrl2 = imgUrl2;
+		this.imgUrl3 = imgUrl3;
+		this.description = description;
+		this.breeds = Arrays.asList(breeds);
+		
+	}
+
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+
+	public String getImgUrl2() {
+		return imgUrl2;
+	}
+
+
+	public String getImgUrl3() {
+		return imgUrl3;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
 
 	public Long getId() {
 		return id;
